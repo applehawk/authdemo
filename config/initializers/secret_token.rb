aec8fe743e01d0ee5145e4d55f437ccf373216e2
deus <deus@deus-desktop.(none)>
2012-07-01 21:09:02 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authdemo::Application.config.secret_token = '55aaa3dcfbbd1c7f18643a8e54125a0cccc85a362fa24c2ce50dca3e220dd72dd0340250f37604008b8d24a0667c408872940b4693b27a5ba96022114403061f'
